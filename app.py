51159e2ebdbf83d467a34b849f65557d5ca3318f
Rajat-264 <handerajat@gmail.com>
2025-03-12 12:15:50 +0530

    Change

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to call backend
@@ -11,4 +12,5 @@ def signup():
     return jsonify({"message": "Signup successful!"})
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=5000)
