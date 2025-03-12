9e211c641bed326ef239d86c17b1e72c4d22d754
Rajat-264 <handerajat@gmail.com>
2025-03-12 11:50:15 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allow frontend to call backend
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.json
+    print("Received data:", data)
+    return jsonify({"message": "Signup successful!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
